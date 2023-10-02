247e8a43abbd50aaa0b48fc721391303fd0121c6
Rojas-Andres <andresfelipe200004@gmail.com>
2023-10-02 16:21:14 -0500

    feat: add column is super admin

--- shared_package/db/models.py
+++ shared_package/db/models.py
@@ -19,6 +19,7 @@ class User(Base):
     full_name = Column(String(200))
     is_active = Column(Boolean, default=False)
     is_admin = Column(Boolean, default=False)
+    is_super_admin = Column(Boolean, default=False)
     password = Column(String(255))
     deleted_at = Column(DateTime, nullable=True, default=None)
 
