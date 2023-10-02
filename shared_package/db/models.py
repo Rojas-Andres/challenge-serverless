5c37dd8dbf953693d6aa57aa073ce0a2ea4710cf
Rojas-Andres <andresfelipe200004@gmail.com>
2023-10-02 15:53:37 -0500

    feat: generate new migrations

--- shared_package/db/models.py
+++ shared_package/db/models.py
@@ -15,9 +15,36 @@ class User(Base):
         autoincrement=True,
         index=True,
     )
-    email = Column(String(200), index=True)
+    email = Column(String(200), index=True, unique=True)
     full_name = Column(String(200))
     is_active = Column(Boolean, default=False)
     is_admin = Column(Boolean, default=False)
     password = Column(String(255))
     deleted_at = Column(DateTime, nullable=True, default=None)
+
+
+class Brands(Base):
+    __tablename__ = "brands"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        index=True,
+    )
+    name = Column(String(200), index=True, unique=True)
+
+
+class Products(Base):
+    __tablename__ = "products"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        index=True,
+    )
+    sku = Column(String(200), index=True, unique=True)
+    name = Column(String(200))
+    price = Column(Float, nullable=False)
+    brand = Column(Integer, ForeignKey("brands.id", ondelete="CASCADE"))
